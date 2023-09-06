9efaddaf34d2c499d322c294596fde4d30fd060d
acfcoder <acfcoder@gmail.com>
2023-09-06 12:30:11 +0200

    ADD v-model & search

--- js/main.js
+++ js/main.js
@@ -3,14 +3,16 @@ const API = "https://api.github.com/users/";
 const app = Vue.createApp ({
     data() {
         return {
-            message: "Hello, Vue"
+            message: "Hello, Vue",
+            search: null
         }
     },
     methods: {
         async doSearch() {
-            const response = await fetch(API + "acfcoder")
+            const response = await fetch(API + this.search)
             const data = await response.json()
             console.log(data)
+            search = null
         }
     }
 })
