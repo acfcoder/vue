354e1ec85658b58210d4805fce3cd1e3679369f6
acfcoder <acfcoder@gmail.com>
2023-09-11 10:23:33 +0200

    Implementación de errores, vistas condicionales y puestas a 0

--- js/main.js
+++ js/main.js
@@ -4,15 +4,28 @@ const app = Vue.createApp ({
     data() {
         return {
             message: "Hello, Vue",
-            search: null
-        }
+            search: null,
+            result: null,
+            error: null
+        };
     },
     methods: {
+       
         async doSearch() {
+            this.result = this.error = null
+           try {
             const response = await fetch(API + this.search)
+            if (!response.ok) throw new Error("User not found")
             const data = await response.json()
             console.log(data)
-            search = null
+            this.result = true
+           } catch (error) {
+            this.error = error
+           } finally {
+            this.search = null
+           }
+           
+            
         }
     }
 })
